16bc8cde6cd729255321bfe8e6c90fed4143a79a
Nathan Chevalier <cerberusgfx@gmail.com>
2023-10-09 10:06:46 -0500

    Swimming animals inheriting class

--- swimming/alligator.py
+++ swimming/alligator.py
@@ -1,18 +1,5 @@
-from datetime import date
-class Alligator:
-
-    def __init__(self, name, species, shift, food):
-        self.name = name
-        self.species = species
-        self.shift = shift
-        self.date_added = date.today()
-        self.walking = True
-        self.food = food
-
-    def __str__(self):
-        return f'{self.name} the {self.species}'
-
-    def feed(self):
-        feed_message = f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}'
-        print(feed_message)
-        return feed_message
+from animal import Animal
+class Alligator(Animal):
+    def __init__(self, name, species, food, chip_num):
+        super().__init__(name, species, food, chip_num)
+        self.swimming = True
